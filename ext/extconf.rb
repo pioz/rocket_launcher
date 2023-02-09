f06a40ecae6695b1f0b736b654fe8a874101ccba
pioz <epilotto@gmx.com>
2023-02-10 00:08:18 +0100

    Add search path for homebrew

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -7,24 +7,21 @@ require 'mkmf'
 INCLUDE_DIRS = [
   RbConfig::CONFIG['includedir'],
   '/usr/local/include',
-  '/usr/include'
+  '/usr/include',
+  '/opt/homebrew/include'
 ]
 
 LIB_DIRS = [
   RbConfig::CONFIG['libdir'],
   '/usr/local/lib',
-  '/usr/lib'
+  '/usr/lib',
+  '/opt/homebrew/lib'
 ]
 
 dir_config('rocket_launcher', INCLUDE_DIRS, LIB_DIRS)
 
-unless find_header('libusb-1.0/libusb.h')
-  abort 'libusb-1.0 is missing.'
-end
-
-unless find_library('usb-1.0', 'libusb_init')
-  abort 'libusb-1.0 is missing.'
-end
+abort 'libusb-1.0 is missing' unless find_header('libusb-1.0/libusb.h')
+abort 'libusb-1.0 is missing' unless find_library('usb-1.0', 'libusb_init')
 
 create_makefile('usb/usb')
 
